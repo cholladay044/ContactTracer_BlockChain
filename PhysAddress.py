eb38acd398e9154e74fe759b1f3309a896a86d56
Cole Holladay <62319431+cholladay044@users.noreply.github.com>
2021-06-10 12:34:37 -0500

    Add files via upload

---
+++ PhysAddress.py
@@ -0,0 +1,14 @@
+class PhysAddress:
+    def __init__(self, street, city, state, zipcode, country):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.country = country
+
+    def update_address(self, street, city, state, zipcode, country):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.country = country
